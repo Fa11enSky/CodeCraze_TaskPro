b43a7d5328867ee71be9d3e3446d454b2217213d
Ivan <133168352+Fa11enSky@users.noreply.github.com>
2024-03-04 21:11:13 +0200

    Revert "Header themes"

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -2,7 +2,6 @@ import Container from 'components/Container/container';
 import { Welcome } from 'components/Welcome/Welcome.styled';
 import WelcomeInner from 'components/Welcome/Welcome';
 import React from 'react';
-import Header from 'components/Header/Header';
 
 const WelcomePage = () => {
   return (
