9771f8b0ec21fba08cfdeb26b5074cef5eac6c1a
LarisaYakovenko <133209659+LarisaYakovenko@users.noreply.github.com>
2024-03-04 19:46:18 +0200

    update style

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -2,6 +2,7 @@ import Container from 'components/Container/container';
 import { Welcome } from 'components/Welcome/Welcome.styled';
 import WelcomeInner from 'components/Welcome/Welcome';
 import React from 'react';
+import Header from 'components/Header/Header';
 
 const WelcomePage = () => {
   return (
