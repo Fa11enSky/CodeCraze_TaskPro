06b0deaa21f47036cb105b0d78eb8f9922816ec1
Vasyl Vintoniv <126397140+vinvas94@users.noreply.github.com>
2024-03-10 18:43:19 +0100

    adddelete

--- src/components/Sidebar/ListBoards/ListBoards.jsx
+++ src/components/Sidebar/ListBoards/ListBoards.jsx
@@ -1,26 +1,22 @@
-import { useState } from 'react';
 import BoardItem from './BoardItem';
 import { ListBoardStyle } from './ListBoards.styled';
-// import { useSelector } from 'react-redux';
-// import { selectedBoard } from 'redux/boards/selectors';
-// import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { selectedBoard } from '../../../redux/boards/selectors';
 
 const ListBoards = ({ boards }) => {
-  const [boardId, setBoardId] = useState(1);
-  // const dispatch=useDispatch()
-  // const test = useSelector(selectedBoard);
-  // console.log(test);
+  const currentBoard = useSelector(selectedBoard);
+
   return (
     <ListBoardStyle>
       <ul>
-        {boards.map(({ title, icon, id }) => (
-          <li
-            key={id}
-            onClick={() => {
-              setBoardId(id);
-            }}
-          >
-            <BoardItem title={title} icon={icon} isActive={id === boardId} />
+        {boards.map(({ title, icon, _id }) => (
+          <li key={_id}>
+            <BoardItem
+              title={title}
+              icon={icon}
+              isActive={_id === currentBoard._id}
+              id={_id}
+            />
           </li>
         ))}
       </ul>
