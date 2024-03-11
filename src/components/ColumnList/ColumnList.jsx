07dfd61418286b2dc4fdd79e90e99d58dfdf7884
Kalugaa <133102595+Kalugaa@users.noreply.github.com>
2024-03-11 20:21:34 +0100

    redux-filter

--- src/components/ColumnList/ColumnList.jsx
+++ src/components/ColumnList/ColumnList.jsx
@@ -1,34 +1,40 @@
-import ColumnItem from '../ColumnItem/ColumnItem';
 import css from './styles.module.css';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 import { fetchSingleBoard } from '../../redux/boards/operationsBoards';
-import { selectedBoard } from '../../redux/boards/selectors';
-import Modal from '../Modal/Modal';
-import FilterModal from 'components/FilterModal/FilterModal';
-import AddColumnButton from 'components/AddColumnButton/AddColumnButton';
-import OpenFiltersButton from 'components/OpenFiltersBtn/OpenFiltersBtn';
+
 import AddColumnModal from '../AddColumnModal/AddColumModal';
+import Modal from '../Modal/Modal';
+import AddColumnButton from '../AddColumnButton/AddColumnButton';
+import OpenFiltersButton from '../OpenFiltersBtn/OpenFiltersBtn';
+import FilterModal from '../FilterModal/FilterModal';
+import { selectFilter, selectedBoard } from '../../redux/boards/selectors';
+import { FilteredColumns } from 'components/FiltredColumns';
+
 const ColumnsList = () => {
   const [isFilterOpen, setIsFilterOpen] = useState(false)
   const board = useSelector(selectedBoard);
+  const filter = useSelector(selectFilter)
   const params = useParams();
   const dispatch = useDispatch();
   const { title, columns, background } = board;
   const bgNumber = background || '1';
   const [isAddColumnOpen, setIsAddColumnOpen] = useState(false);
+
   const toggleAddColumn = () => {
-  setIsAddColumnOpen(!isAddColumnOpen)
+    setIsAddColumnOpen(!isAddColumnOpen)
   }
   const toggleFilter = () => {
+
     setIsFilterOpen(!isFilterOpen);
   };
-  
+
   useEffect(() => {
     dispatch(fetchSingleBoard(params.boardId));
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
 
   const isRetina = () => {
     if (window.devicePixelRatio > 1) {
@@ -50,9 +56,8 @@ const ColumnsList = () => {
 
   const device = setDevice();
   const ratio = isRetina();
-  const bgurl = require(`../../assets/backgrounds/allBg/${device}_background_${
-    bgNumber + ratio
-  }.jpg`);
+  const bgurl = require(`../../assets/backgrounds/allBg/${device}_background_${bgNumber + ratio
+    }.jpg`);
 
   return (
     <div
@@ -61,30 +66,32 @@ const ColumnsList = () => {
     >
       <div className={css.headerWrapper}>
         <h4 className={css.board_title}>{title}</h4>
-        <OpenFiltersButton click={toggleFilter}/>
+        <OpenFiltersButton click={toggleFilter} />
       </div>
       {board.columns && board.columns[0]._id ? (
         <>
           <ul className={css.column_list}>
-            {columns.map(el => {
-              return <ColumnItem key={el._id} column={el} />;
-            })}
+
+            <FilteredColumns columns={{ columns }} filter={filter} />
             <li>
               <AddColumnButton click={toggleAddColumn} />
             </li>
           </ul>
         </>
       ) : (
-        <AddColumnButton click={toggleAddColumn}/>
+        <AddColumnButton click={toggleAddColumn} />
       )}
       {isAddColumnOpen && (
-          <Modal onClose={toggleAddColumn}>
-            <AddColumnModal onClose={toggleAddColumn} />
-          </Modal>
+        <Modal onClose={toggleAddColumn}>
+          <AddColumnModal onClose={toggleAddColumn} />
+        </Modal>
       )}
-      {isFilterOpen && <Modal onClose={toggleFilter}><FilterModal onClose={ toggleFilter} /></Modal>}
+      {isFilterOpen && <Modal onClose={toggleFilter}><FilterModal onClose={toggleFilter} /></Modal>}
     </div>
+
   );
 };
 
 export default ColumnsList;
+
+
