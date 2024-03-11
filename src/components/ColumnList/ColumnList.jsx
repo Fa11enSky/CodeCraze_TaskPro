006761c9cf4edbf3c7f2b30facd694ead6e00ac2
Fa11enSky <iwanpomirsky@gmail.com>
2024-03-11 19:51:25 +0200

    Revert "Merge pull request #76 from Fa11enSky/revert-73-fix-pages"
    This reverts commit 511c4d41d62f72f29de6de4ebbeddbd647e963e0, reversing
    changes made to dfbe2e19fcdd84b49c03c7b3946ff584a0ca1e56.

--- src/components/ColumnList/ColumnList.jsx
+++ src/components/ColumnList/ColumnList.jsx
@@ -4,11 +4,17 @@ import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 import { fetchSingleBoard } from '../../redux/boards/operationsBoards';
+import { selectedBoard } from '../../redux/boards/selectors';
+import AddColumnModal from '../AddColumnModal/AddColumModal';
+import Modal from '../Modal/Modal';
+import AddColumnButton from '../AddColumnButton/AddColumnButton';
+import OpenFiltersButton from '../OpenFiltersBtn/OpenFiltersBtn';
+import FilterModal from '../FilterModal/FilterModal';
 import { selectFilter, selectedBoard } from '../../redux/boards/selectors';
-import AddColumnModal from 'components/AddColumnModal/AddColumnModal';
-import Modal from 'components/Modal/Modal';
 import { setFilter } from '../../redux/boards/filterSlice';
+
 const ColumnsList = () => {
+  const [isFilterOpen, setIsFilterOpen] = useState(false)
   const board = useSelector(selectedBoard);
   const filter = useSelector(selectFilter)
   const params = useParams();
@@ -17,9 +23,15 @@ const ColumnsList = () => {
   const bgNumber = background || '1';
   const [isAddColumnOpen, setIsAddColumnOpen] = useState(false);
   const toggleAddColumn = () => {
+
     setIsAddColumnOpen(!isAddColumnOpen)
   }
-
+  const toggleFilter = () => {
+    setIsFilterOpen(!isFilterOpen)
+  }
+  
+    setIsAddColumnOpen(!isAddColumnOpen)
+  }
 
   useEffect(() => {
     dispatch(fetchSingleBoard(params.boardId));
@@ -62,7 +74,10 @@ const ColumnsList = () => {
       className={css.task_list_container}
       style={{ backgroundImage: `url(${bgurl})` }}
     >
-      <h4 className={css.board_title}>{title}</h4>
+      <div className={css.headerWrapper}>
+        <h4 className={css.board_title}>{title}</h4>
+        <OpenFiltersButton click={toggleFilter}/>
+      </div>
       {board.columns && board.columns[0]._id ? (
         <>
           <ul className={css.column_list}>
@@ -70,23 +85,19 @@ const ColumnsList = () => {
               return <ColumnItem key={el._id} column={el} />;
             })}
             <li>
-              <button
-                onClick={toggleAddColumn}
-                style={{ width: 335, height: 56 }}
-              >
-                add column
-              </button>
+              <AddColumnButton click={toggleAddColumn} />
             </li>
           </ul>
         </>
       ) : (
-        <button onClick={toggleAddColumn}>addColumn</button>
+        <AddColumnButton click={toggleAddColumn}/>
       )}
       {isAddColumnOpen && (
         <Modal onClose={toggleAddColumn}>
           <AddColumnModal onClose={toggleAddColumn} />
         </Modal>
       )}
+      {isFilterOpen && <Modal onClose={toggleFilter}><FilterModal onClose={ toggleFilter} /></Modal>}
     </div>
   );
 };
