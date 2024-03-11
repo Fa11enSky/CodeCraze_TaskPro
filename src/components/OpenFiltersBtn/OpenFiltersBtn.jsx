f0f0aa3466f35ea52ae61b5fbe823b656addccdf
Vitalii1973 <protsyukv@ukr.net>
2024-03-11 17:38:59 +0200

    1111

---
+++ src/components/OpenFiltersBtn/OpenFiltersBtn.jsx
@@ -0,0 +1,39 @@
+// OpenFiltersButton.jsx
+import React, { useState } from 'react';
+import sprite from '../../assets/svgSprite/iconsSprite.svg';
+import { Wrapper, Button, SvgIcon, Text } from './ColumnFiltersStyles';
+import ColumnFiltersModal from './ColumnFiltersModal';
+// import { Container } from './ColumnFiltersModal';
+
+const OpenFiltersButton = ({ onFilterPriority }) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClick = () => {
+    console.log('handleClick function called');
+    if (typeof onFilterPriority === 'function') {
+      onFilterPriority();
+    }
+    setShowModal(true);
+    console.log(showModal);
+    console.log('Opening modal');
+  };
+
+  return (
+    <Wrapper>
+      <Button onClick={handleClick}>
+        <SvgIcon>
+          <use xlinkHref={`${sprite}#icon-filter`} width={16} height={16} />
+        </SvgIcon>
+        <Text>Filters</Text>
+      </Button>
+
+      {/* Відображення модального вікна */}
+      <ColumnFiltersModal
+        isOpen={showModal}
+        onClose={() => setShowModal(false)}
+      />
+    </Wrapper>
+  );
+};
+
+export default OpenFiltersButton;
